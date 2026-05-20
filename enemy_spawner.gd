231d49291b65561a34199e53963bfe26d0bc1b2b
JustALover <fahikun@gmail.com>
2026-05-20 13:41:25 -0500

    Asteroid spawners finished
    Terminé como funcionan los spawners, funcionan relativamente bien y dentro del juego se ven bastante bien, muchas veces los asteroides se sobreponen entre ellos, pero nada grave que no se pueda arreglar luego.
    Además de eso, probando las físicas y todo es mejor dejar los asteroides sin físicas, ya que con físicas causan UN MONTÓN de problemas

--- enemy_spawner.gd
+++ enemy_spawner.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
@@ -18,8 +18,33 @@ func _on_timer_timeout() -> void:
 	var opciones = [asteroid_grey, asteroid_white]
 	var elegido = opciones.pick_random()
 	var asteroid = elegido.instantiate()
-	add_child(asteroid)
 	if elegido == asteroid_grey:
 		print("Asteroide Gris")
 	else:
 		print("Asteroide Blanco")
+	var screen_size = get_viewport_rect().size
+	var center = screen_size / 2
+	var lado = randi() % 4
+
+	match lado:
+		0:
+			asteroid.position = Vector2(randf_range(0, screen_size.x), -50)
+
+		1:
+			asteroid.position = Vector2(randf_range(0, screen_size.x), screen_size.y + 50)
+
+		2:
+			asteroid.position = Vector2(-50, randf_range(0, screen_size.y))
+
+		3:
+			asteroid.position = Vector2(screen_size.x + 50, randf_range(0, screen_size.y))
+
+	# Dirección hacia el centro
+	var direction = (center - asteroid.position).normalized()
+
+	# Pequeña variación random
+	direction = direction.rotated(deg_to_rad(randf_range(-25, 25)))
+
+	asteroid.direction = direction
+
+	add_child(asteroid)
