4099c8aa111e958138409d5872f8828bdbecff69
JustALover <fahikun@gmail.com>
2026-05-22 12:50:58 -0500

    added Pause menu
    -Se añadió un menú de pausa y despausa en el juego al presionar ESC.
    -Se añadió una imagen con el logo de godot mostrar visualmente que el juego está pausado.

---
+++ Escenas/Game/pause_menu.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+#Pause Menu
+var paused := false
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		paused = !paused
+		get_tree().paused = paused
+		
+		$PauseImage.visible = paused
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
