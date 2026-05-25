d304b9499db5a647a482c765582612d84b9715fe
JustALover <fahikun@gmail.com>
2026-05-25 09:56:04 -0500

    Added: Main title and play button
    -Se añadió al juego el menú principal y el botón de jugar
    -Ahora el jugador cada que entre al juego será recibido por el menú principal y por un botón de "PLAY"
    -El botón "PLAY"  tiene animaciones de:
    +Hover
    +UnHover
    +Press
    +Unpress
    -Se movieron algunos archivos de lugar para crear más orden en el desarrollo
    -Se crearon carpetas especificas para cada textura del juego

---
+++ Escenas/Buttons/play_button_title.gd
@@ -0,0 +1,33 @@
+extends TextureButton
+
+var pressedd := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_mouse_entered() -> void:
+	$AnimationsPlayButton.play("Hover")
+	pressedd = false
+
+func _on_mouse_exited() -> void:
+	if pressedd == true:
+		pass
+	else:
+		$AnimationsPlayButton.play("DesHover")
+
+
+func _on_button_down() -> void:
+	$AnimationsPlayButton.play("Pressed")
+	pressedd = true
+	get_tree().change_scene_to_file("res://Escenas/Game/ass_steroids.tscn")
+
+
+func _on_button_up() -> void:
+	$AnimationsPlayButton.play("RESET")
--- Escenas/Game/pause_menu.gd
+++ Escenas/Game/pause_menu.gd
@@ -9,6 +9,7 @@ func _input(event):
 		get_tree().paused = paused
 		
 		$PauseImage.visible = paused
+		print("Juego ha sido pausado o despausado")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
