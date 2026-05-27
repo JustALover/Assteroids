769843bf02e6d2366e87c61b8da7fd6b8ee24a10
JustALover <fahikun@gmail.com>
2026-05-27 05:38:33 -0500

    Enhanced: Main Title Focus Behaviour
    -Now the focus on the main title buttons only activates if the player press: "ui_up" or "ui_down"

--- Escenas/Assteroids Title Screen Menu/main_title_assteroids.gd
+++ Escenas/Assteroids Title Screen Menu/main_title_assteroids.gd
@@ -4,13 +4,15 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$MilanoPaLantePaTraAnim.play("Milano (Pa-lante Pa-tra)")
-	$PanelContainer/HBoxContainer/LeftContainer/ButtonsPlayerContainer/PlayButton.grab_focus()
+	
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_down"):
+		if get_viewport().gui_get_focus_owner() == null:
+			$PanelContainer/HBoxContainer/LeftContainer/ButtonsPlayerContainer/PlayButton.grab_focus()
 
 
 func _on_play_button_pressed() -> void:
